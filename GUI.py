83a0eebc45f3f4b9fb5ca98852ec53efac039f2e
BuildTools <unconfigured@null.spigotmc.org>
2021-08-01 17:27:35 +0400

    added more boards

--- GUI.py
+++ GUI.py
@@ -1,21 +1,15 @@
 # GUI.py
 import pygame
+import random
+from boards import *
 import time
+
 pygame.font.init()
 
 
 class Grid:
-    board = [
-        [7, 8, 0, 4, 0, 0, 1, 2, 0],
-        [6, 0, 0, 0, 7, 5, 0, 0, 9],
-        [0, 0, 0, 6, 0, 1, 0, 7, 8],
-        [0, 0, 7, 0, 4, 0, 2, 6, 0],
-        [0, 0, 1, 0, 5, 0, 9, 3, 0],
-        [9, 0, 4, 0, 6, 0, 0, 0, 5],
-        [0, 7, 0, 3, 0, 0, 0, 1, 2],
-        [1, 2, 0, 0, 0, 7, 4, 0, 0],
-        [0, 4, 9, 2, 0, 6, 0, 0, 7]
-    ]
+    
+    board = random.choice([b1, b2, b3])
 
     def __init__(self, rows, cols, width, height, win):
         self.rows = rows
